7a536fc01da41cd290c7d5ab77477ea6b3e109f9
Ubuntu <ubuntu@ip-172-31-28-41.us-west-1.compute.internal>
2024-02-27 01:41:04 +0000

    initial commit

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+from datetime import date
+
+app = Flask(__name__)
+
+@app.route("/")
+def resume_home():
+    return render_template("index.html")
+
+
+@app.route("/about")
+def about_me():
+    return render_template("about_me.html")
+
+def todays_date():
+    today = date.today()
+    str_date = today.strftime("%B %d %Y")
+    return "Today's date is " + str_date
